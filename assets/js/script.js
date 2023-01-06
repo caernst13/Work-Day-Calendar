f0e3ae013221035891acec12002e412833f08cd0
caernst13 <caleb_ernst@yahoo.com>
2023-01-05 21:26:25 -0500

    added code to change background color of tasks based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
-var hour1 = document.getElementById("nine")
-var hour2 = document.getElementById("ten")
-var hour3 = document.getElementById("eleven")
-var hour4 = document.getElementById("noon")
-var hour5 = document.getElementById("one")
-var hour6 = document.getElementById("two")
-var hour7 = document.getElementById("three")
-var hour8 = document.getElementById("four")
-var hour9 = document.getElementById("five")
+var hour0 = document.getElementById("nine")
+var hour1 = document.getElementById("ten")
+var hour2 = document.getElementById("eleven")
+var hour3 = document.getElementById("noon")
+var hour4 = document.getElementById("one")
+var hour5 = document.getElementById("two")
+var hour6 = document.getElementById("three")
+var hour7 = document.getElementById("four")
+var hour8 = document.getElementById("five")
+
+var blocks = [hour0, hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8];
 
 var today=moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
-var hour = moment().hours();
-console.log(hour)
-
+// var time = moment().hours() - 9;
+// console.log(time)
+for (let i = 0; i<9; i++) {
+    if (time > i) {
+        blocks[i].classList.add("past")
+    } else if (time === i) {
+        blocks[i].classList.add("present")
+    } else {
+        blocks[i].classList.add("future")
+    }
+}
 
 
 
