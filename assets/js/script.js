86e616d8aa813af91c835dc268ea72218496d65d
caernst13 <caleb_ernst@yahoo.com>
2023-01-06 01:33:25 -0500

    added local storage for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,32 @@ var form6 = document.getElementById("hour6");
 var form7 = document.getElementById("hour7");
 var form8 = document.getElementById("hour8");
 
+var button0 = document.getElementById("btn0")
+var button1 = document.getElementById("btn1")
+var button2 = document.getElementById("btn2")
+var button3 = document.getElementById("btn3")
+var button4 = document.getElementById("btn4")
+var button5 = document.getElementById("btn5")
+var button6 = document.getElementById("btn6")
+var button7 = document.getElementById("btn7")
+var button8 = document.getElementById("btn8")
 
-// form5.placeholder='testing'
+var toDo0 = '';
+var toDo1 = '';
+var toDo2 = '';
+var toDo3 = '';
+var toDo4 = '';
+var toDo5 = '';
+var toDo6 = '';
+var toDo7 = '';
+var toDo8 = '';
+
+// var start = document.querySelector('.container')
+
+// form5.placeholder='log'
 var blocks = [hour0, hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8];
 var tasks = [form0, form1, form2, form3, form4, form5, form6, form7, form8];
+var works = [toDo0, toDo1, toDo2, toDo3, toDo4, toDo5, toDo6, toDo7, toDo8]
 
 var today=moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
@@ -39,9 +61,62 @@ for (let i = 0; i<9; i++) {
     }
 }
 
-// for (let k = 0; k<9; k++) {
-//     blocks[k].addEventListener('submit', console.log('test'))
-// }
 
 
-form0.addEventListener('click', console.log("what"))
+var log0 = function(event) {
+    event.preventDefault();
+    job0=form0.value
+    localStorage.setItem("task0", job0)
+}
+var log1 = function(event) {
+    event.preventDefault();
+    job1=form1.value
+    localStorage.setItem("task1", job1)
+}
+var log2 = function(event) {
+    event.preventDefault();
+    job2=form2.value
+    localStorage.setItem("task2", job2)
+}
+var log3 = function(event) {
+    event.preventDefault();
+    job3=form3.value
+    localStorage.setItem("task3", job3)
+}
+var log4 = function(event) {
+    event.preventDefault();
+    job4=form4.value
+    localStorage.setItem("task4", job4)
+}
+var log5 = function(event) {
+    event.preventDefault();
+    job5=form5.value
+    localStorage.setItem("task5", job5)
+}
+var log6 = function(event) {
+    event.preventDefault();
+    job6=form6.value
+    localStorage.setItem("task6", job6)
+}
+var log7 = function(event) {
+    event.preventDefault();
+    job7=form7.value
+    localStorage.setItem("task7", job7)
+}
+var log8 = function(event) {
+    event.preventDefault();
+    job8=form8.value
+    localStorage.setItem("task8", job8)
+}
+
+
+button0.addEventListener('click', log0);
+button1.addEventListener('click', log1);
+button2.addEventListener('click', log2);
+button3.addEventListener('click', log3);
+button4.addEventListener('click', log4);
+button5.addEventListener('click', log5);
+button6.addEventListener('click', log6);
+button7.addEventListener('click', log7);
+button8.addEventListener('click', log8);
+
