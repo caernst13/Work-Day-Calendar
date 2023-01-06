0b5e1b05f1dc6fa010a0f30c9c436167e54b2a9a
caernst13 <caleb_ernst@yahoo.com>
2023-01-05 21:04:34 -0500

    added selectors for all the hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
+var hour1 = document.getElementById("nine")
+var hour2 = document.getElementById("ten")
+var hour3 = document.getElementById("eleven")
+var hour4 = document.getElementById("noon")
+var hour5 = document.getElementById("one")
+var hour6 = document.getElementById("two")
+var hour7 = document.getElementById("three")
+var hour8 = document.getElementById("four")
+var hour9 = document.getElementById("five")
+
 var today=moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
 var hour = moment().hours();
+console.log(hour)
 
 
 
